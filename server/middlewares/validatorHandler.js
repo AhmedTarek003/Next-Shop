c985f9d22b2e527cdc9a6425720a694770fac182
ahemd003 <at3616464@gmail.com>
2024-12-15 13:39:49 +0200

    first commit

---
+++ server/middlewares/validatorHandler.js
@@ -0,0 +1,15 @@
+const { validationResult } = require("express-validator");
+const fs = require("node:fs");
+
+const validatorHandler = (req, res, next) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    if (req.file) {
+      fs.unlinkSync(req.file.path);
+    }
+    return res.status(400).json({ msg: result.array()[0].msg });
+  }
+  next();
+};
+
+module.exports = validatorHandler;
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const errorHandler = require("./middlewares/errorHandling");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+
+connectDB();
+const app = express();
+
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
+
+app.use("*", () => {
+  throw new Error("Invalid route");
+});
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () =>
+  console.log(`server is running on port ${PORT}`)
+);
+process.on("unhandledRejection", (err) => {
+  console.log(`unhandled rejection error || ${err.name} : ${err.message}`);
+  server.close(() => {
+    console.log("shutting down");
+    process.exit(1);
+  });
+});
