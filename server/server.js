2e6edff57e06a7412cb89a6ca91cadab5b365325
ahemd003 <at3616464@gmail.com>
2025-01-29 19:42:40 +0200

    upload promocode

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ app.use("/api/v1/products", require("./routes/productRoute"));
 app.use("/api/v1/categories", require("./routes/categoryRoute"));
 app.use("/api/v1/reviews", require("./routes/reviewRoute"));
 app.use("/api/v1/sliders", require("./routes/sliderRoute"));
+app.use("/api/v1/promocodes", require("./routes/promocodeRoute"));
 
 app.use("*", () => {
   throw new Error("Invalid route");
